9b5b47c93930d1265139231169dcf3e670dd37eb
BibekPoudel855 <bibekpoudel855@gmail.com>
2026-04-09 11:39:49 +0545

    Complete: added folder structure & setup cdn for tailwind css

--- src/main/java/com/neospark/neohub/HelloServlet.java
+++ src/main/java/com/neospark/neohub/HelloServlet.java
@@ -2,27 +2,21 @@ package com.neospark.neohub;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        response.setContentType("text/html");
+//
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+        request.getRequestDispatcher("/WEB-INF/views/common/header.jsp").forward(request, response);
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
 }
---
+++ src/main/webapp/WEB-INF/views/common/header.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: NITRO
+  Date: 4/9/2026
+  Time: 9:52 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>NEO-HUB</title>
+</head>
+<body>
+<h1 class="font-bold text-2xl text-red-200">this is jsp for tailwind config</h1>
+<script src="https://cdn.tailwindcss.com"></script>
+</body>
+</html>
