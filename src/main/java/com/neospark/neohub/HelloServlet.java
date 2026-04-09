1fdbf9b86e06ea8f3af4e92bab7402294d517393
BibekPoudel855 <bibekpoudel855@gmail.com>
2026-04-09 23:02:37 +0545

    build: added cdn libraries in one file header.jsp

--- src/main/java/com/neospark/neohub/HelloServlet.java
+++ src/main/java/com/neospark/neohub/HelloServlet.java
@@ -9,13 +9,6 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-//        response.setContentType("text/html");
-//
-//        // Hello
-//        PrintWriter out = response.getWriter();
-//        out.println("<html><body>");
-//        out.println("<h1>" + message + "</h1>");
-//        out.println("</body></html>");
         request.getRequestDispatcher("/WEB-INF/views/common/header.jsp").forward(request, response);
     }
 
